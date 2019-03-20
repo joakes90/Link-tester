effddfd69e93b27076e6cef43c91473caab7752a
joakes90 <justin.t.oakes@gmail.com>
2019-03-20 14:07:54 -0500

    doneish

--- Link Launcher/ViewController.m
+++ Link Launcher/ViewController.m
@@ -41,7 +41,7 @@
                                                     NSString *newURL = self.addURLAlert.textFields[0].text;
                                                     [self.URLs addObject:newURL];
                                                     [self saveURLsinArray:self.URLs];
-                                                    // TODO: Add to Table View
+                                                    [self insertURL:newURL];
                                                 }];
 
     [alert addAction:cancel];
@@ -51,6 +51,12 @@
     [self presentViewController:alert animated:YES completion:nil];
 }
 
+- (void) insertURL: (NSString *) url {
+    [self.tableView setEditing:YES];
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:self.URLs.count - 1 inSection:0];
+    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationRight];
+    [self.tableView setEditing:NO];
+}
 - (NSMutableArray *)savedURLs {
     NSArray *urls = [[NSUserDefaults standardUserDefaults] arrayForKey:@"URLS"];
     return [urls mutableCopy];
@@ -73,5 +79,39 @@
     [cell.textLabel setText:url];
 
     return cell;
+}
+
+#pragma mark TableView Delegate
+
+- (NSArray<UITableViewRowAction *> *)tableView:(UITableView *)tableView editActionsForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewRowAction *action = [UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleDestructive
+                                                                      title:@"Delete"
+                                                                    handler:^(UITableViewRowAction * _Nonnull action, NSIndexPath * _Nonnull indexPath) {
+                                                                        [self.URLs removeObjectAtIndex:indexPath.row];
+                                                                        [self saveURLsinArray:self.URLs];
+                                                                        [self.tableView deleteRowsAtIndexPaths:@[indexPath]
+                                                                                              withRowAnimation:UITableViewRowAnimationLeft];
+                                                                    }];
+    return @[action];
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+    NSURL *url = [NSURL URLWithString:self.URLs[indexPath.row]];
+    [[UIApplication sharedApplication] openURL:url
+                                       options:@{}
+                             completionHandler:^(BOOL success) {
+                                 if (!success){
+                                     UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Could not open URL"
+                                                                                                    message:@"Maybe the supporting application isn't installed"
+                                                                                             preferredStyle:UIAlertControllerStyleAlert];
+                                     UIAlertAction *action = [UIAlertAction actionWithTitle:@"Ok"
+                                                                                      style:UIAlertActionStyleDefault
+                                                                                    handler:^(UIAlertAction * _Nonnull action) {}];
+                                     [alert addAction:action];
+                                     [self presentViewController:alert animated:YES completion:^{
+                                     }];
+                                 }
+                             }];
 }
 @end
